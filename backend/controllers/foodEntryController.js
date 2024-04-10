66487e1e3be62157f987dc6e1e4b32a55c5d0dca
Aitan Bachrach <bachrachar@gmail.com>
2024-04-09 21:27:02 -0400

    food entry controller

---
+++ backend/controllers/foodEntryController.js
@@ -0,0 +1,18 @@
+const {deleteFoodEntry, createFoodEntry, getFoodEntry} = require("../models/foodEntryModel")
+
+const foodEntryController = {};
+
+foodEntryController.getFoodEntryData = async (req, res) => {
+  result = await getFoodEntry(req.query)
+  res.status(result.status).json(result.json)
+};
+
+foodEntryController.createFoodEntryData = async (req, res) => {
+  result = await createFoodEntry(req.body.date, req.body.name, req.body.parts, req.body.userId)
+  res.status(result.status).json(result.json)
+};
+
+foodEntryController.deleteFoodEntryData = async (req, res) => {
+  result = await deleteFoodEntry(req.params.id)
+  res.status(result.status).json(result.json)
+}; 
