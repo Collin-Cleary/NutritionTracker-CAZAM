57908f9c673355bcb3e277ae1a5e9ac6d23974ec
Aitan Bachrach <bachrachar@gmail.com>
2024-04-11 20:36:33 -0400

    we will be finding this

--- frontend/models/databaseFetcher.js
+++ frontend/models/databaseFetcher.js
@@ -11,14 +11,19 @@ class FoodSearch {
   }
   
   async fetchForData(query, pageNumber, sortBy) {
+    console.log("We will be seeing where this fails")
     const queryString = new URLSearchParams({api_key : key, query : query, pageNumber : pageNumber, sortBy : sortBy}).toString();
+    console.log("1")
     try {
+      console.log("2")
       const response = await fetch(databaseURL+`?${queryString}`)
+      console.log("3")
       if (!response.ok){
-      throw new Error("Failed to Fetch data")
+        throw new Error("Failed to Fetch data")
       }
+      console.log("4")
       this.searchResults = await response.json()
-
+      console.log("5")
     } catch (error) {
       console.error("Failed to fetch: ", error.message)
     }
