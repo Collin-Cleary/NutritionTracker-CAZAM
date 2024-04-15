33f5fc65162adb73f253f70d706df2e5dbcefc71
MUSKAN SHAH <muskanshah@MUSKANs-MacBook-Air.local>
2024-04-14 23:07:26 -0400

    Updated App.js

--- frontend/frontend_nutritiontracker/src/App.js
+++ frontend/frontend_nutritiontracker/src/App.js
@@ -2,33 +2,95 @@ import React, { useState } from 'react';
 import './App.css';
 import Login from './Login';
 import Home from './Home';
+import axios from "axios";
+import {findAllByPlaceholderText} from "@testing-library/react";
+
+const baseUrl = "http://localhost:5000";
+
+const checkSession = () => {
+  return new Promise((resolve, reject) => {
+    const data = {
+      "id": localStorage.getItem('id'),
+      "access_token": localStorage.getItem('access_token')
+    };
+
+    axios.post(`${baseUrl}/auth/check_session`, JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+        .then(response => {
+          if (response.data.session_active) {
+            console.log('User already logged in');
+            resolve(true);
+          } else {
+            console.log('User logged out');
+            resolve(false);
+          }
+        })
+        .catch(error => {
+          console.log(`Error fetching data from the server: `, error);
+          resolve(false);
+        });
+  });
+}
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  checkSession().then(sessionActive => {
+    console.log(sessionActive);
+    setIsLoggedIn(sessionActive);
+  });
   const handleLogin = () => {
     setIsLoggedIn(true);
   };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
+  const logout = () => {
+    axios.post(
+        `${baseUrl}/auth/logout`,
+        JSON.stringify({ "id": localStorage.getItem('id') }),
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+    )
+        .then(response => {
+          if (response.status === 200) {
+            console.log('User logged out successfully');
+            localStorage.removeItem('access_token');
+            setIsLoggedIn(false);
+            console.log('LOGGED OUT');
+          } else {
+            console.log('User logout failed');
+          }
+        })
+        .catch(error => {
+          console.log(`Error fetching data from the server: `, error);
+        });
+  };
+  const handleLogout = (e) => {
+    logout();
+    e.preventDefault();
   };
 
   return (
     <div className="App">
 
       <header>
-        <div className="header-left">
+        <div class="header-left">
           <h1>NutriKit</h1>
         </div>
-        <div className="header-right">
+        <div class="header-right">
           <nav>
             <ul>
-              { isLoggedIn ? (
+              {/*<li>Home</li>*/}
+              {/*<li>About</li>*/}
+              {/*<li>Contact</li>*/}
+              { isLoggedIn && (
                 <li>
                   <button className="nav-link" onClick={handleLogout}>Logout</button>
                 </li>
-              ) : null }
+              )}
             </ul>
           </nav>
         </div>
