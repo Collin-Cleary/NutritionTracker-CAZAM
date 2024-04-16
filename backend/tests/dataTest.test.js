9b87ce2c07e4db09c6ab34bb70e382138c450fa7
MUSKAN SHAH <muskanshah@MUSKANs-MacBook-Air.local>
2024-04-16 13:31:02 -0400

    removing code smells

--- backend/tests/dataTest.test.js
+++ backend/tests/dataTest.test.js
@@ -13,20 +13,21 @@ describe('Unit Tests', () => {
     });
 
     it('should login with correct credentials and return JWT token', () => {
-      try {
-        const validCredentials = { userName: 'john_doe', password: 'password123' }; // NOSONAR
-        const req = { body: validCredentials }; // Mock request object
-        const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
-        const user = { userName: validCredentials.userName };
-        sinon.stub(User, 'findOne').resolves(user);
-        sinon.stub(bcrypt, 'compare').resolves(true);
-        sinon.stub(jwt, 'sign').returns('mockToken');
-
-        authController.login(req, res);
-        assert(res.status.calledOnceWithExactly(200));
-        assert(res.json.calledOnceWithExactly({ token: 'mockToken' }));
-      } catch (error) {
-        console.log(error);
+      try{
+          const validCredentials = { userName: 'john_doe', password: 'password123' }; // NOSONAR
+          const req = { body: validCredentials }; // Mock request object
+          const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
+          const user = { userName: validCredentials.userName };
+          sinon.stub(User, 'findOne').resolves(user);
+          sinon.stub(bcrypt, 'compare').resolves(true);
+          sinon.stub(jwt, 'sign').returns('mockToken');
+    
+          authController.login(req, res);
+    
+          assert(res.status.calledOnceWithExactly(200));
+          assert(res.json.calledOnceWithExactly({ token: 'mockToken' }));
+      } catch(error){
+        console.log(error)
       }
     });
 
@@ -36,74 +37,95 @@ describe('Unit Tests', () => {
       const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
       sinon.stub(User, 'findOne').resolves(null);
       sinon.stub(bcrypt, 'compare').resolves(false);
+
       authController.login(req, res);
+
       assert(res.status.calledOnceWithExactly(401));
       assert(res.json.calledOnceWithExactly({ message: 'Invalid username or password' }));
     });
   });
 
-  describe('Logout API', () => {
-    afterEach(() => {
-      sinon.restore();
-    });
-
-    it('should return success message for logout', () => {
-      const req = {}; // Mock request object
-      const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
-
-      authController.logout(req, res);
-      assert(res.status.calledOnceWithExactly(200));
-      assert(res.json.calledOnceWithExactly({ message: 'Logout successful' }));
-    });
-  });
-
-  describe('Profile Creation API', () => {
-    afterEach(() => {
+    describe('Logout API', () => {
+      afterEach(() => {
         sinon.restore();
       });
-    it('should create a new profile successfully', async () => {
-      const newProfileData = {
-        userName: 'john_doe',
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: 'password123', // NOSONAR
-        confirmPassword: 'password123',
-        height: 180,
-        weight: 75
-      };
-      const req = { body: newProfileData }; // Mock request object
-      const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
-      sinon.stub(User, 'findOne').resolves(null);
-      sinon.stub(bcrypt, 'hash').resolves('hashedPassword');
-      sinon.stub(User.prototype, 'save').resolves();
-      await authController.createProfile(req, res);
-      assert(res.status.calledOnceWithExactly(201));
-      assert(res.json.calledOnceWithExactly({ message: 'Profile created successfully' }));
-    });
 
-    it('should return 400 if passwords do not match', async () => {
-      const invalidProfileData = {
-        name: 'Test User',
-        email: 'test@example.com',
-        confirmPassword: 'password456', // Different password
-      };
-      const req = { body: invalidProfileData }; // Mock request object
-      const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
-      assert(res.status.calledOnceWithExactly(400));
-      assert(res.json.calledOnceWithExactly({ message: 'Passwords do not match' }));
-    });
+      it('should return success message for logout', () => {
+        const req = {}; // Mock request object
+        const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
+      
+        authController.logout(req, res);
+      
+        assert(res.status.calledOnceWithExactly(200));
+        assert(res.json.calledOnceWithExactly({ message: 'Logout successful' }));
+      });
 
-    it('should return 400 if user already exists with the provided email', async () => {
-      const existingProfileData = {
-        userName: 'existing_user',
-        name: 'Existing User',
-        height: 170,
-        weight: 70
-      };
-      const req = { body: existingProfileData }; // Mock request object
-      const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
-      sinon.stub(User, 'findOne').resolves({ email: 'test@example.com' });
-      assert(res.json.calledOnceWithExactly({ message: 'User already exists with this email' }));
     });
+    
+
+    describe('Profile Creation API', () => {
+      afterEach(() => {
+        sinon.restore();
+      });
+      it('should create a new profile successfully', async () => {
+        const newProfileData = {
+          userName: 'john_doe',
+          name: 'John Doe',
+          email: 'johndoe@example.com',
+          password: 'password123', // NOSONAR
+          confirmPassword: 'password123',
+          height: 180,
+          weight: 75
+        };
+        const req = { body: newProfileData }; // Mock request object
+        const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
+        sinon.stub(User, 'findOne').resolves(null);
+        sinon.stub(bcrypt, 'hash').resolves('hashedPassword');
+        sinon.stub(User.prototype, 'save').resolves();
+
+        await authController.createProfile(req, res);
+
+        assert(res.status.calledOnceWithExactly(201));
+        assert(res.json.calledOnceWithExactly({ message: 'Profile created successfully' }));
+      });
+
+      it('should return 400 if passwords do not match', async () => {
+        const invalidProfileData = {
+          name: 'Test User',
+          email: 'test@example.com',
+          password: 'password123', // NOSONAR
+          confirmPassword: 'password456', // Different password
+          height: 180,
+          weight: 75
+        };
+        const req = { body: invalidProfileData }; // Mock request object
+        const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
+
+        await authController.createProfile(req, res);
+
+        assert(res.status.calledOnceWithExactly(400));
+        assert(res.json.calledOnceWithExactly({ message: 'Passwords do not match' }));
+      });
+
+      it('should return 400 if user already exists with the provided email', async () => {
+        const existingProfileData = {
+          userName: 'existing_user',
+          name: 'Existing User',
+          email: 'test@example.com',
+          password: 'password123', // NOSONAR
+          confirmPassword: 'password123',
+          height: 170,
+          weight: 70
+        };
+        const req = { body: existingProfileData }; // Mock request object
+        const res = { status: sinon.stub().returnsThis(), json: sinon.stub() }; // Mock response object
+        sinon.stub(User, 'findOne').resolves({ email: 'test@example.com' });
+
+        await authController.createProfile(req, res);
+
+        assert(res.status.calledOnceWithExactly(400));
+        assert(res.json.calledOnceWithExactly({ message: 'User already exists with this email' }));
+      });
+
   });
 });
