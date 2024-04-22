8767df4e788f9c752b9f2712683e6a9020b4a8c3
Aitan Bachrach <bachrachar@gmail.com>
2024-04-22 19:53:23 -0400

    diet fixes

--- frontend/frontend_nutritiontracker/src/DietTracker.js
+++ frontend/frontend_nutritiontracker/src/DietTracker.js
@@ -10,45 +10,48 @@ function DietTracker({ consumedCalories, setConsumedCalories}) {
   const [foodData, setFoodData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const username = localStorage.getItem('userName');
-      const date = new Date().toISOString().slice(0, 10);
-      const url = `http://localhost:5000/api/calorie/${username}/${date}`;
-      try {
-        const response = await axios.get(url);
-        if (response.data) {
-          setConsumedCalories(response.data[0].intake);
-          
-        }
-      } catch (error) {
-        console.log('Error fetching data:', error);
-      }
-      
-      try {
-        // get food data
-        const foodResponse = await axios.get('http://localhost:5000/api/fooditem', {
-          params: { userId:username} 
-        });
-        
-        if (foodResponse.data) {
-          
-          const transformedData = foodResponse.data.map(item => ({
-            name: item.name,
-            calories: item.nutrition.calories 
-          }));
-          
-          setFoodData(transformedData);
-          
-        }
-      } catch (foodError) {
-        console.log('Error fetching food data:', foodError);
-      }
-    };
-
+    console.log('useEffect called');
     
 
     fetchData();
-  }, [foodData, setConsumedCalories, setFoodData]);
+  }, []);
+
+  const fetchData = async () => {
+    await fetchCaloriesData();
+    await fetchFoodData();
+  };
+
+  const fetchCaloriesData = async () => {
+    const username = localStorage.getItem('userName');
+    const date = new Date().toISOString().slice(0, 10);
+    const url = `http://localhost:5000/api/calorie/${username}/${date}`;
+    try {
+      const response = await axios.get(url);
+      if (response.data) {
+        setConsumedCalories(response.data[0].intake);
+      }
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  };
+
+  const fetchFoodData = async () => {
+    const username = localStorage.getItem('userName');
+    try {
+      const foodResponse = await axios.get('http://localhost:5000/api/fooditem', {
+        params: { userId: username }
+      });
+      if (foodResponse.data) {
+        const transformedData = foodResponse.data.map(item => ({
+          name: item.name,
+          calories: item.nutrition.Calories
+        }));
+        setFoodData(transformedData);
+      }
+    } catch (foodError) {
+      console.log('Error fetching food data:', foodError);
+    }
+  };
 
   const updateConsumedCalories = async (intake) => {
     const username = localStorage.getItem('userName');
